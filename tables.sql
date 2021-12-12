977279b08d17341d265efe65f3cc0292c56ae050
ericyu18 <ericyu18@uw.edu>
2021-12-11 17:29:54 -0800

    Qtaro

--- tables.sql
+++ tables.sql
@@ -1,21 +1,21 @@
-CREATE TABLE items (
-  itemid INTEGER PRIMARY KEY,
-  itemName TEXT,
-  imagePath TEXT,
-  lore TEXT,
-  price INTEGER,
-  capacity INTEGER,
-  itemType TEXT
-);
+-- CREATE TABLE items (
+--   itemid INTEGER PRIMARY KEY,
+--   itemName TEXT,
+--   imagePath TEXT,
+--   lore TEXT,
+--   price INTEGER,
+--   capacity INTEGER,
+--   itemType TEXT
+-- );
 
-CREATE TABLE users (
-  userid INTEGER PRIMARY KEY,
-  username TEXT,
-  email TEXT,
-  passcode TEXT,
-  balance INTEGER,
-  sessionid TEXT
-);
+-- CREATE TABLE users (
+--   userid INTEGER PRIMARY KEY,
+--   username TEXT,
+--   email TEXT,
+--   passcode TEXT,
+--   balance INTEGER,
+--   sessionid TEXT
+-- );
 
 CREATE TABLE orders (
   orderid INTEGER PRIMARY KEY,
@@ -27,13 +27,13 @@ CREATE TABLE orders (
   foreign key (itemid) references items(itemid)
 );
 
-CREATE TABLE ratings (
-  ratingid INTEGER PRIMARY KEY,
-  userid INTEGER,
-  itemid INTEGER,
-  ratingDate DATE DEFAULT (datetime('now','localtime')),
-  stars INTEGER,
-  comment TEXT,
-  foreign key (userid) references users(userid),
-  foreign key (itemid) references items(itemid)
-);
+-- CREATE TABLE ratings (
+--   ratingid INTEGER PRIMARY KEY,
+--   userid INTEGER,
+--   itemid INTEGER,
+--   ratingDate DATE DEFAULT (datetime('now','localtime')),
+--   stars INTEGER,
+--   comment TEXT,
+--   foreign key (userid) references users(userid),
+--   foreign key (itemid) references items(itemid)
+-- );
