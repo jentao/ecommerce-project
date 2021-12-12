0609e9cc07c65120a4190032314e17c720af848d
ericyu18 <ericyu18@uw.edu>
2021-12-11 17:29:02 -0800

    Qtaro

--- tables.sql
+++ tables.sql
@@ -1,30 +1,31 @@
--- CREATE TABLE items (
---   itemid INTEGER PRIMARY KEY,
---   itemName TEXT,
---   imagePath TEXT,
---   lore TEXT,
---   price INTEGER,
---   capacity INTEGER,
---   itemType TEXT
--- );
+CREATE TABLE items (
+  itemid INTEGER PRIMARY KEY,
+  itemName TEXT,
+  imagePath TEXT,
+  lore TEXT,
+  price INTEGER,
+  capacity INTEGER,
+  itemType TEXT
+);
 
--- CREATE TABLE users (
---   userid INTEGER PRIMARY KEY,
---   username TEXT,
---   email TEXT,
---   passcode TEXT,
---   balance INTEGER,
---   sessionid TEXT
--- );
+CREATE TABLE users (
+  userid INTEGER PRIMARY KEY,
+  username TEXT,
+  email TEXT,
+  passcode TEXT,
+  balance INTEGER,
+  sessionid TEXT
+);
 
--- CREATE TABLE orders (
---   orderid INTEGER PRIMARY KEY,
---   userid INTEGER,
---   itemid INTEGER,
---   orderDate DATE DEFAULT (datetime('now','localtime')),
---   foreign key (userid) references users(userid),
---   foreign key (itemid) references items(itemid)
--- );
+CREATE TABLE orders (
+  orderid INTEGER PRIMARY KEY,
+  userid INTEGER,
+  itemid INTEGER,
+  orderDate DATE DEFAULT (datetime('now','localtime')),
+  rated INTEGER,
+  foreign key (userid) references users(userid),
+  foreign key (itemid) references items(itemid)
+);
 
 CREATE TABLE ratings (
   ratingid INTEGER PRIMARY KEY,
